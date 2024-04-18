5ea77d5fba29767edf2bedcf3c9a2b80feb6217e
evilBetooxx <albertof4in@gmail.com>
2024-04-18 08:50:45 -0600

    Terminado

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+
+function App() {
+  const socket = io("http://localhost:3002");
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Conectado al servidor de sockets");
+    });
+
+    socket.on("payment", (payment) => {
+      const paymentAmount = parseInt(payment.total);
+      alert("Pago recibido:" + paymentAmount);
+    });
+  }, []);
+
+  const handleOnClick = async () => {
+    try {
+      await axios.post("http://localhost:3000/order", {
+        amount: 100,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center text-2xl justify-center bg-black text-white font-bold h-screen">
+      <div className="p-10">Hola, Mundo!</div>
+      <div className="p-10">
+        <button
+          className="p-10 bg-slate-500 rounded-md"
+          onClick={handleOnClick}
+        >
+          Haz una compra
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
